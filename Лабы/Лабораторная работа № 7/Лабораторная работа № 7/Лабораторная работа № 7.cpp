91bf874fc66a1b467c04f04a90d43ecddedf686e
Wuffluv <rodion.rycha@yandex.ru>
2023-05-16 09:23:45 +0900

    Final docs

---
+++ Лабы/Лабораторная работа № 7/Лабораторная работа № 7/Лабораторная работа № 7.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <chrono>
+
+using namespace std::chrono;
+
+// Шаблонная функция для обмена двух элементов
+template<typename T>
+void swap(T& a, T& b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+// Шаблонная функция для сортировки пузырьком
+template<typename T>
+void bubbleSort(T arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+// Шаблонная функция для сортировки выбором
+template<typename T>
+void selectionSort(T arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        swap(arr[min_idx], arr[i]);
+    }
+}
+
+// Шаблонная функция для сортировки вставками
+template<typename T>
+void insertionSort(T arr[], int n)
+{
+    for (int i = 1; i < n; i++) {
+        T key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+// Функция для генерации случайных массивов заданного размера
+int* generateRandomArray(int n)
+{
+    int* arr = new int[n];
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand();
+    }
+    return arr;
+}
+
+// Функция для тестирования работы функций сортировки
+void testSortFunctions()
+{
+    int arr1[] = { 5, 2, 1, 6, 8 };
+    int n1 = sizeof(arr1) / sizeof(arr1[0]);
+    bubbleSort(arr1, n1);
+    std::cout << "Bubble sort: ";
+    for (int i = 0; i < n1; i++) {
+        std::cout << arr1[i] << " ";
+    }
+    std::cout << std::endl;
+
+    int arr2[] = { 5, 2, 1, 6, 8 };
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
+    selectionSort(arr2, n2);
+    std::cout << "Selection sort: ";
+    for (int i = 0; i < n2; i++) {
+        std::cout << arr2[i] << " ";
+    }
+    std::cout << std::endl;
+
+    int arr3[] = { 5, 2, 1, 6, 8 };
+    int n3 = sizeof(arr3) / sizeof(arr3[0]);
+    insertionSort(arr3, n3);
+    std::cout << "Insertion sort: ";
+    for (int i = 0; i < n3; i++) {
+        std::cout << arr3[i] << " ";
+    }
+    std::cout << std::endl;
+}
---
+++ Лабы/Лабораторная работа №6/Лабораторная работа №6/Лабораторная работа №6.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+
+using namespace std;
+
+// Шаблонная функция поиска по ключу в динамическом массиве
+template <typename T>
+int search(T* arr, int size, T key)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == key)
+            return i;
+    }
+    return -1; // Если элемент не найден
+}
+
+
+int main()
+{
+    
+    setlocale(LC_ALL, "Russian");
+    // Тестирование на малых массивах
+    int intArr[] = { 1, 2, 3, 4, 5 };
+    int intSize = sizeof(intArr) / sizeof(int); // Размер массива
+    int intKey = 3;
+    int intIndex = search(intArr, intSize, intKey);
+    cout << "Индекс элемента " << intKey << " в массиве: " << intIndex << endl;
+
+    double doubleArr[] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+    int doubleSize = sizeof(doubleArr) / sizeof(double);
+    double doubleKey = 4.4;
+    int doubleIndex = search(doubleArr, doubleSize, doubleKey);
+    cout << "Индекс элемента " << doubleKey << " в массиве: " << doubleIndex << endl;
+
+    // Тестирование на массивах, заполненных случайными числами
+    const int arrSize = 500;
+    int intRand[arrSize];
+    double doubleRand[arrSize];
+
+    // Заполнение массивов случайными числами
+    srand(time(NULL));
+    for (int i = 0; i < arrSize; i++)
+    {
+        intRand[i] = rand() % 100;
+        doubleRand[i] = (double)(rand() % 100) / 10;
+    }
+
+    // Поиск элемента по ключу в массивах
+    int intRandKey = intRand[rand() % arrSize];
+    double doubleRandKey = doubleRand[rand() % arrSize];
+
+    clock_t start = clock(); // Запуск таймера
+    int intRandIndex = search(intRand, arrSize, intRandKey);
+    double doubleRandIndex = search(doubleRand, arrSize, doubleRandKey);
+    clock_t end = clock(); // Остановка таймера
+
+    // Вывод результатов
+
+    cout << "Индекс элемента " << intRandKey << " в массиве intRand: " << intRandIndex << endl;
+    cout << "Индекс элемента " << doubleRandKey << " в массиве doubleRand: " << doubleRandIndex << endl;
+    cout << "Время выполнения функций: " << (double)(end - start) / CLOCKS_PER_SEC << " секунд" << endl;
+    cout << "Массив intArr: ";
+    for (int i = 0; i < intSize; i++)
+    {
+        cout << intArr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Массив doubleArr: ";
+    for (int i = 0; i < doubleSize; i++)
+    {
+        cout << doubleArr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Массив intRand: ";
+    for (int i = 0; i < arrSize; i++)
+    {
+        cout << intRand[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Массив doubleRand: ";
+    for (int i = 0; i < arrSize; i++)
+    {
+        cout << doubleRand[i] << " ";
+    }
+    cout << endl;
+
+    // Запись результатов в файл
+    ofstream fout("result.txt");
+    fout << "Результаты тестирования функции поиска по ключу в динамических массивах" << endl;
+    fout << "Индекс элемента " << intKey << " в массиве intArr: " << intIndex << endl;
+    fout << "Индекс элемента " << doubleKey << " в массиве doubleArr: " << doubleIndex << endl;
+    fout << "Индекс элемента " << intRandKey << " в массиве intRand: " << intRandIndex << endl;
+    fout << "Индекс элемента " << doubleRandKey << " в массиве doubleRand: " << doubleRandIndex << endl;
+    fout << "Время выполнения функций: " << (double)(end - start) / CLOCKS_PER_SEC << " секунд" << endl;
+    fout.close(); 
+    return 0;
+}

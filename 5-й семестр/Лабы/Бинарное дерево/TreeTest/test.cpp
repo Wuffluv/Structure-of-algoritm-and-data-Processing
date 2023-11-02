038fd98e7bd14e3392842a9b408cc983163ff98d
Wuffluv <rodion.rycha@yandex.ru>
2023-11-02 09:16:45 +0900

    Null

--- 5-й семестр/Лабы/Бинарное дерево/TreeTest/test.cpp
+++ 5-й семестр/Лабы/Бинарное дерево/TreeTest/test.cpp
@@ -1,6 +1,58 @@
-#include "pch.h"
+#include <iostream>
+#include "ClassTreeNode.h"
+#include <gtest/gtest.h>
+#include <pch.h>
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace std;
+
+// Тест для функции подсчета узлов
+TEST(TreeTest, CountNodesTest) {
+    Tree tree;
+    EXPECT_EQ(0, countNodes(tree.getRoot())); // Проверяем пустое дерево
+
+    // Добавляем элементы в дерево
+    tree.add(10);
+    tree.add(5);
+    tree.add(15);
+    tree.add(2);
+    tree.add(7);
+    tree.add(12);
+
+    EXPECT_EQ(6, countNodes(tree.getRoot())); // Проверяем корректное подсчет узлов в дереве
+}
+
+// Тест для функции определения глубины дерева
+TEST(TreeTest, DepthTest) {
+    Tree tree;
+    EXPECT_EQ(0, height(tree.getRoot())); // Проверяем глубину пустого дерева
+
+    // Добавляем элементы в дерево
+    tree.add(10);
+    tree.add(5);
+    tree.add(15);
+    tree.add(2);
+    tree.add(7);
+    tree.add(12);
+
+    EXPECT_EQ(3, height(tree.getRoot())); // Проверяем корректное определение глубины дерева
+}
+
+// Тест для функции удаления дерева
+TEST(TreeTest, DestroyTreeTest) {
+    Tree tree;
+    // Добавляем элементы в дерево
+    tree.add(10);
+    tree.add(5);
+    tree.add(15);
+    tree.add(2);
+    tree.add(7);
+    tree.add(12);
+
+    tree.destroyTree(tree.getRoot());
+    EXPECT_EQ(nullptr, tree.getRoot()); // Проверяем, что дерево удалено
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }

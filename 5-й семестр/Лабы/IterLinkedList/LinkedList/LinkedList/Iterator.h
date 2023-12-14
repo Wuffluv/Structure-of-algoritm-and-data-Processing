4c23a51ca0bd923a95f4d66369510742b5b203f5
Wuffluv <rodion.rycha@yandex.ru>
2023-12-14 11:34:01 +0900

    создание класса Node

--- 5-й семестр/Лабы/IterLinkedList/LinkedList/LinkedList/Iterator.h
+++ 5-й семестр/Лабы/IterLinkedList/LinkedList/LinkedList/Iterator.h
@@ -4,14 +4,14 @@
 
 
 template <typename T>
-class AbstractIterator {
+class Iterator {
 public:
     virtual T& operator*() = 0; //возвращает ссылку на текущий элемент итератора
-    virtual AbstractIterator<T>& operator++() = 0; //передвигает итератор на следующий элемент (префиксный инкремент)
-    virtual AbstractIterator<T>& operator++(int) = 0; // передвигает итератор на следующий элемент и возвращает старое значение (постфиксный инкремент)
-    virtual bool operator==(const AbstractIterator<T>& other) = 0; // сравнивает два итератора на равенство
-    virtual bool operator!=(const AbstractIterator<T>& other) = 0; // сравнивает два итератора на неравенство
-    virtual ~AbstractIterator() {} // деструктор
+    virtual Iterator<T>& operator++() = 0; //передвигает итератор на следующий элемент (префиксный инкремент)
+    virtual Iterator<T>& operator++(int) = 0; // передвигает итератор на следующий элемент и возвращает старое значение (постфиксный инкремент)
+    virtual bool operator==(const Iterator<T>& other) = 0; // сравнивает два итератора на равенство
+    virtual bool operator!=(const Iterator<T>& other) = 0; // сравнивает два итератора на неравенство
+    virtual ~Iterator() {} // деструктор
 
    
 };
--- 5-й семестр/Лабы/IterLinkedList/LinkedList/LinkedList/LinkedList.h
+++ 5-й семестр/Лабы/IterLinkedList/LinkedList/LinkedList/LinkedList.h
@@ -1,3 +1,19 @@
 //@author Rychkov R.V.
-
 #pragma once
+#include <iostream>
+#include "Iterator.h"
+
+template <typename T>
+class Node {
+public:
+	T data; //Данные хранящиеся в узле
+	Node* next; //указатель на след. узел в списке
+	Node* prev; //укзаатель на пред. узел в списке
+
+	//конструктор
+	Node(int data) {
+		this->data = data; //активация данных узла
+		next = nullptr; //активация указателя след. узла
+		prev = nullptr; //активация указателя пред. узла
+	}
+};
